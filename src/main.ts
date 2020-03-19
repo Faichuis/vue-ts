d68cc07246c697fcb899c67164c5cf76ef8724ef
FeiCui <465766401@qq.com>
2020-03-20 01:55:01 +0800

    111

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import store from './store'
 import "@/assets/scss/common.scss"
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import _ from "lodash"
 
 Vue.use(ElementUI);
 
